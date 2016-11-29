e75998b82643120d5c37b56da4f5dd76dfb80f61
Daniel <daniel.maansson@gmail.com>
2016-11-29 09:10:45 +0100

    Initial commit

---
+++ Assets/timers.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class timers : MonoBehaviour
+{
+	List<Text> m_timers;
+	public RectTransform m_image;
+	public RectTransform m_toggle;
+	int seq = 0;
+
+	void Start ()
+	{
+		m_timers = new List<Text>(GetComponentsInChildren<Text>());
+	}
+	
+	void LateUpdate ()
+	{
+		foreach (var t in m_timers)
+		{
+			t.text = seq + "\n" +
+				Time.realtimeSinceStartup;
+		}
+
+		seq = (seq + 1) % 1000;
+
+		m_image.localPosition = Vector3.down * (10f + 10f * (seq % 6));
+
+		if (Input.anyKeyDown)
+		{
+			m_toggle.gameObject.SetActive(!m_toggle.gameObject.activeSelf);
+		}
+	}
+}
